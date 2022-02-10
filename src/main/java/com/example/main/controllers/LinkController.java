739ee0f637cdf557da0914f06c91861669ada009
Ikaraz <m.djuka@hotmail.com>
2022-02-10 10:57:44 +0100

    Added LinkController

---
+++ src/main/java/com/example/main/controllers/LinkController.java
@@ -0,0 +1,30 @@
+package com.example.main.controllers;
+
+import com.example.main.models.LinkModel;
+import com.example.main.services.LinkService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@CrossOrigin
+@RestController
+public class LinkController {
+    @Autowired
+    private LinkService linkService;
+
+    @GetMapping("/links")
+    public List<LinkModel> findAll(){
+        return linkService.findAll();
+    }
+
+    @PostMapping("/links")
+    public LinkModel saveOne(@RequestBody LinkModel link){
+        return linkService.saveOne(link);
+    }
+
+    @DeleteMapping("/links/{id}")
+    public void deleteOne(@PathVariable int id){
+        linkService.deleteOne(id);
+    }
+}
