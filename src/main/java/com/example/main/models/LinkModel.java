569de7ffda008d2bda822af6c2f43b61abf507bc
Ikaraz <m.djuka@hotmail.com>
2022-02-10 10:46:25 +0100

    Added LinkModel

--- src/main/java/com/example/main/models/LinkModel.java
+++ src/main/java/com/example/main/models/LinkModel.java
@@ -27,4 +27,44 @@ public class LinkModel {
         this.klijent_id = klijent_id;
         this.zahtev_id = zahtev_id;
     }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getOdgovor_teme() {
+        return odgovor_teme;
+    }
+
+    public void setOdgovor_teme(String odgovor_teme) {
+        this.odgovor_teme = odgovor_teme;
+    }
+
+    public int getKlijent_id() {
+        return klijent_id;
+    }
+
+    public void setKlijent_id(int klijent_id) {
+        this.klijent_id = klijent_id;
+    }
+
+    public int getZahtev_id() {
+        return zahtev_id;
+    }
+
+    public void setZahtev_id(int zahtev_id) {
+        this.zahtev_id = zahtev_id;
+    }
 }
