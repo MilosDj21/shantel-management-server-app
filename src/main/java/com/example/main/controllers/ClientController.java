3b54fe69ec2af6f312d85423b9c9a4f7eab5b0e5
Ikaraz <m.djuka@hotmail.com>
2022-02-10 10:32:16 +0100

    Added ClientController

---
+++ src/main/java/com/example/main/controllers/ClientController.java
@@ -0,0 +1,31 @@
+package com.example.main.controllers;
+
+import com.example.main.models.ClientModel;
+import com.example.main.services.ClientService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@CrossOrigin
+@RestController
+public class ClientController {
+
+    @Autowired
+    private ClientService clientService;
+
+    @GetMapping("/clients")
+    public List<ClientModel> findAll(){
+        return clientService.findAll();
+    }
+
+    @PostMapping("/clients")
+    public ClientModel saveOne(@RequestBody ClientModel client){
+        return clientService.saveOne(client);
+    }
+
+    @DeleteMapping("/clients/{id}")
+    public void deleteOne(@PathVariable int id){
+        clientService.deleteOne(id);
+    }
+}
--- src/main/java/com/example/main/repositories/ClientRepository.java
+++ src/main/java/com/example/main/repositories/ClientRepository.java
@@ -2,7 +2,9 @@ package com.example.main.repositories;
 
 import com.example.main.models.ClientModel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ClientRepository extends JpaRepository<ClientModel, Integer> {
 
 }
