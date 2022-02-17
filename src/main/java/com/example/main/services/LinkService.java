3c97f5c27534f315749597e0476a395dfa49526e
Ikaraz <m.djuka@hotmail.com>
2022-02-17 14:11:12 +0100

    Added findAllByCLientId method

--- src/main/java/com/example/main/services/LinkService.java
+++ src/main/java/com/example/main/services/LinkService.java
@@ -1,26 +1,43 @@
 package com.example.main.services;
 
+import com.example.main.models.ClientModel;
 import com.example.main.models.LinkModel;
 import com.example.main.repositories.LinkRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class LinkService {
     @Autowired
     private LinkRepository linkRepository;
+    @Autowired
+    private ClientService clientService;
+
 
     public List<LinkModel> findAll(){
         return linkRepository.findAll();
+    }
+
+    public List<LinkModel> findAllByClientId(long clientId){
+        List<LinkModel> list = new ArrayList<>();
+        ClientModel c = clientService.findById(clientId);
+        List<LinkModel> list1 = findAll();
+        for(LinkModel l: list1){
+            if(l.getKlijent().equals(c)){
+                list.add(l);
+            }
+        }
+        return list;
     }
 
     public LinkModel saveOne(LinkModel link){
         return linkRepository.save(link);
     }
 
-    public void deleteOne(long id){
+    public void deleteById(long id){
         linkRepository.deleteById(id);
     }
 }
