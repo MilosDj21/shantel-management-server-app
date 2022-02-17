68d871ba1ee5443b2ccdd547620e1cdaca7db714
Ikaraz <m.djuka@hotmail.com>
2022-02-17 14:11:35 +0100

    Added findById and findAllByUserId methods

--- src/main/java/com/example/main/services/RequestService.java
+++ src/main/java/com/example/main/services/RequestService.java
@@ -1,10 +1,14 @@
 package com.example.main.services;
 
 import com.example.main.models.RequestModel;
+import com.example.main.models.UserModel;
 import com.example.main.repositories.RequestRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -12,16 +16,34 @@ public class RequestService {
 
     @Autowired
     private RequestRepository requestRepository;
+    @Autowired
+    private UserService userService;
 
     public List<RequestModel> findAll(){
         return requestRepository.findAll();
+    }
+
+    public RequestModel findById(long id){
+        return requestRepository.findById(id).orElse(null);
+    }
+
+    public List<RequestModel> findAllByUserId(long userId){
+        List<RequestModel> list = new ArrayList<>();
+        UserModel u = userService.findById(userId);
+        List<RequestModel> list1 = findAll();
+        for(RequestModel r: list1){
+            if(r.getKorisnik().equals(u)){
+                list.add(r);
+            }
+        }
+        return list;
     }
 
     public RequestModel saveOne(RequestModel requestModel){
         return requestRepository.save(requestModel);
     }
 
-    public void deleteOne(long id){
+    public void deleteById(long id){
         requestRepository.deleteById(id);
     }
 
