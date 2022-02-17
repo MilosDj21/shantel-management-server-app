ac26e9399a9c81359b3bc52951662df8de343351
Ikaraz <m.djuka@hotmail.com>
2022-02-17 13:59:13 +0100

    Added saveOne method

--- src/main/java/com/example/main/controllers/LinkController.java
+++ src/main/java/com/example/main/controllers/LinkController.java
@@ -1,9 +1,13 @@
 package com.example.main.controllers;
 
+import com.example.main.models.ClientModel;
 import com.example.main.models.LinkModel;
+import com.example.main.services.ClientService;
 import com.example.main.services.LinkService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
@@ -12,19 +16,32 @@ import java.util.List;
 public class LinkController {
     @Autowired
     private LinkService linkService;
+    @Autowired
+    private ClientService clientService;
 
     @GetMapping("/links")
     public List<LinkModel> findAll(){
         return linkService.findAll();
     }
 
-    @PostMapping("/links")
-    public LinkModel saveOne(@RequestBody LinkModel link){
-        return linkService.saveOne(link);
+    @GetMapping("/clients/{clientId}/links")
+    public List<LinkModel> findAllByClientId(@PathVariable long clientId){
+        return linkService.findAllByClientId(clientId);
+    }
+
+    @PostMapping("/clients/{clientId}/links")
+    public LinkModel saveOne(@PathVariable long clientId, @RequestBody LinkModel linkModel){
+        ClientModel c = clientService.findById(clientId);
+        if(c == null){
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }else {
+            linkModel.setKlijent(c);
+            return linkService.saveOne(linkModel);
+        }
     }
 
     @DeleteMapping("/links/{id}")
     public void deleteOne(@PathVariable long id){
-        linkService.deleteOne(id);
+        linkService.deleteById(id);
     }
 }
